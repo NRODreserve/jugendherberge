6c4938fcb7c099922225978d37f5b93b355da79a
noah.rodriguez-el-hardouzi@student.htl-rankweil.at <noah.rodriguez-el-hardouzi@student.htl-rankweil.at>
2024-11-10 10:07:18 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -14,10 +14,24 @@ class Form1(Form1Template):
     # Any code you write here will run before the form opens.
     
     print(anvil.server.call("get_jugendherbergen"))
-    self.drop_down_1.items = anvil.server.call('get_jugendherbergen', "name, JID")
+    jugendherbergen = anvil.server.call('get_jugendherbergen', "name, JID")
+    self.drop_down_1.items = jugendherbergen
 
     print(anvil.server.call("get_preiskategorie_for_jugendherbergen"))
     self.drop_down_2.items = anvil.server.call('get_preiskategorie_for_jugendherbergen', "name, PID")
 
+  
+    jid_to_name = {jid: name for name, jid in jugendherbergen}
+
     print(anvil.server.call("get_zimmer_for_jugendherbergen"))
-    self.drop_down_3.items = anvil.server.call('get_zimmer_for_jugendherbergen', "zimmernummer, bettenanzahl, preis_pro_nacht, gebucht, JID, ZID")
+    self.listezimmer = [(zimmernummer, bettenanzahl, preis_pro_nacht, gebucht, JID, ZID) for zimmernummer, bettenanzahl, preis_pro_nacht, gebucht, JID, ZID in anvil.server.call('get_zimmer_for_jugendherbergen')]
+
+    liste = []
+    
+    for i in self.listezimmer:
+     jugendherberge_name = jid_to_name.get(i[5], "Unknown")
+     status = "Verfügbar" if not i[4] else "Gebucht"
+     liste.append(f"Zimmernummer: {i[1]}, Bettenanzahl: {i[2]}, Preis: {i[3]}€, Jugendherberge: {jugendherberge_name}, Verfügbarkeitsstatus: {status}")
+      
+    
+    self.drop_down_3.items = liste 
